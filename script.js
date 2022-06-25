654097b7a1c684e5a73da98732d9403fbcce8bab
Jozif Mikhael <jozifmikhael@gmail.com>
2022-06-25 19:36:16 -0400

    added basic functionality

--- script.js
+++ script.js
@@ -10,7 +10,20 @@ gridButton.addEventListener('click', () => {
             gridSize = prompt("Min is 16x16");
     }
     createGrid(gridSize);
+    start();
 })
+
+function start(){
+    // hover ('mouseover') event listener for each square
+    let squares = document.querySelectorAll('.square');
+
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', (event) => {
+            console.log(event.target);
+            square.setAttribute('style', 'background-color: black');
+        })
+    });
+}
 
 let gridContainer = document.querySelector('#grid-container');
 function createGrid(gridSize){
@@ -25,11 +38,10 @@ function createGrid(gridSize){
         for (let col = 0; col < gridSize; col++){
             let newDiv = document.createElement('div');
             newDiv.classList.toggle('square');
+            newDiv.setAttribute('style', `max-width: ${960 / gridSize}px; height: ${960 / gridSize}px;`);
             rowDiv.appendChild(newDiv);
         }
         // firstElementChild to return full element child (firstChild returns comment node)
         gridContainer.insertBefore(rowDiv, gridContainer.firstElementChild);
     }
 }
-
-
